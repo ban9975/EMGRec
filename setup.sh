ac54088fa371982f3eb5861c0ee475b35cd1afc3
ban9975 <b06901013@ntu.edu.tw>
2021-12-19 18:15:52 +0800

    upload setup.sh

---
+++ setup.sh
@@ -0,0 +1,15 @@
+sudo apt-get update && sudo apt-get upgrade
+
+# setting aliases
+echo -e "alias lab='cd ~/Desktop/EMGRec'\n" >> ~/.bash_aliases
+source ~/.bashrc
+
+# adc
+pip install adafruit-circuitpython-ads1x15
+cd ~/Desktop
+git clone https://github.com/adafruit/Adafruit_CircuitPython_ADS1x15.git
+cp Adafruit_CircuitPython_ADS1x15/examples/ads1x15_simpletest.py ~/Desktop/gestureRec/adc.py
+
+# matplotlib
+apt-get install libatlas-base-dev
+pip install matplotlib
