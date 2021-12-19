4656711ad0927281c3763219604da05d07e55eb3
ban9975 <b06901013@ntu.edu.tw>
2021-12-19 22:25:45 +0800

    setup.sh

--- setup.sh
+++ setup.sh
@@ -1,15 +1,22 @@
-sudo apt-get update && sudo apt-get upgrade
+#!/bin/bash
 
-# setting aliases
-echo -e "alias lab='cd ~/Desktop/EMGRec'\n" >> ~/.bash_aliases
-source ~/.bashrc
+sudo apt update
+sudo apt full-upgrade -y
+sudo apt-get update
+sudo apt-get upgrade
+sudo apt-get install vim -y
 
 # adc
 pip install adafruit-circuitpython-ads1x15
 cd ~/Desktop
 git clone https://github.com/adafruit/Adafruit_CircuitPython_ADS1x15.git
-cp Adafruit_CircuitPython_ADS1x15/examples/ads1x15_simpletest.py ~/Desktop/gestureRec/adc.py
+cp Adafruit_CircuitPython_ADS1x15/examples/ads1x15_simpletest.py ~/Desktop/EMGRec/adc.py
 
 # matplotlib
-apt-get install libatlas-base-dev
+sudo apt-get install libatlas-base-dev
 pip install matplotlib
+pip install -U numpy
+
+# setting aliases
+echo -e "alias lab='cd ~/Desktop/EMGRec'\n" >> ~/.bash_aliases
+source ~/.bashrc
