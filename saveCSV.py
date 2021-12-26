6aee2b3d55b26225989110caadea04eb45d6f9a9
ban9975 <b06901013@ntu.edu.tw>
2021-12-26 19:44:30 +0800

    add saveCSV

--- saveCSV.py
+++ saveCSV.py
@@ -1,7 +1,30 @@
+import sys
 import csv
-with open('output.csv', 'w', newline='') as csvfile:
+import time
+import board
+import busio
+import adafruit_ads1x15.ads1015 as ADS
+from adafruit_ads1x15.analog_in import AnalogIn
+
+if len(sys.argv) >= 1:
+	fileName = sys.argv[1] + '.csv'
+else:
+	fileName = 'output.csv'
+
+if len(sys.argv) >= 2:
+	duration = int(sys.argv[2])
+else:
+	# default collect data for 5 seconds
+	duration = 5
+
+start = time.time()
+i2c = busio.I2C(board.SCL, board.SDA)
+ads = ADS.ADS1015(i2c)
+chaUp = AnalogIn(ads, ADS.P0)
+chaDown = AnalogIn(ads, ADS.P1)
+
+with open(fileName, 'w', newline='') as csvfile:
 	writer = csv.writer(csvfile)
 	writer.writerow(['up', 'down'])
-
-	for i in range(10):
-		writer.writerow([i, 2*i])
+	while time.time() - start < duration:
+		writer.writerow([chaUp.value, chaDown.value])
